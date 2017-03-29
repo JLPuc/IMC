718d825e007f26e10fce247905b8a23917fd37b0
JoseLuisPucChan <josechan211@gmail.com>
2017-03-29 04:00:20 -0600

    Codificación
    -Primera parte de la códificación
    -Creación de clases
    -Recuperación de los datos  y calculo de los resultados obtenidos

--- IMC_CalculoPeso/app/src/main/java/com/mastercoder/imc_calculopeso/Informacion.java
+++ IMC_CalculoPeso/app/src/main/java/com/mastercoder/imc_calculopeso/Informacion.java
@@ -1,13 +1,26 @@
 package com.mastercoder.imc_calculopeso;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Informacion extends AppCompatActivity {
 
+    Button btnCalcular;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        //Redirige a la ventana de Calculo de IMC
+        Button btnCalcular = (Button) findViewById(R.id.btnCalculo);
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), CalculoIMC.class);
+                startActivity(i);
+            }
+        });
     }
 }
