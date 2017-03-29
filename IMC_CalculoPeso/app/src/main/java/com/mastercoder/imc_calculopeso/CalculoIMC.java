958bce2219b5475d53c09dfe97f425fed4abc630
Satiago281295 <josemartinez.1h@gmail.com>
2017-03-29 00:53:06 -0600

    Actualización de aplicación móvil
    - Creación de pantallas
    * Pantalla principal
    * Pantalla de información
    * Pantalla para el cálculo del IMC

---
+++ IMC_CalculoPeso/app/src/main/java/com/mastercoder/imc_calculopeso/CalculoIMC.java
@@ -0,0 +1,13 @@
+package com.mastercoder.imc_calculopeso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculoIMC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imc);
+    }
+}
